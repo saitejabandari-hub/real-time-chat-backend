b8a7372b65d31c84304ec0e769f90c5692c8db3b
saitejabandari-hub <bandarisaitej9@gmail.com>
2026-07-04 23:53:51 +0530

    added connecteduser disconnected acknoledgment

--- server.js
+++ server.js
@@ -15,26 +15,43 @@ const io = new Server(server,{
     }
 }) // it tells create socket.io server and attach it to our HTTP server
 
-io.on("connection",(socket)=>{ // event call when someone entires, each user has there own socketId which is unique, socket.id identify connection not usename
+const users = [] // stores usere who joined 
+
+io.on("connection",(socket)=>{ // event call's when someone entires, each user has there own socketId which is unique, socket.id identify connection not usename
 
      console.log("User Connected:", socket.id);
      
 
-    socket.on("send_message", (data) => {
+    socket.on("send_message", (data,callback) => {  // send data by calling event
 
-    console.log("Sender Socket ID:", socket.id);
+    console.log("Sender Socket ID:", socket.id); 
     console.log("Sender Username:", data.username);
 
-    socket.to(data.room).emit("receive_message",data) // send data to every one in the room
+    callback({
+        success:"Successful"
+    })
+
+    io.to(data.room).emit("receive_message",data) // send data to every one in the room
     });
 
     socket.on("join_room",(data)=>{
         socket.join(data.room)  // join the room 
+
+        users.push({
+            socketId : socket.id,
+            username : data.username,
+            room : data.room 
+        })
+
+        const roomUsers = users.filter(each => each.room === data.room)
+
+        io.to(data.room).emit("room_users",roomUsers)
+      
         socket.to(data.room).emit("user_joined",data)
     })
 
     socket.on("typing",(data)=>{
-        io.to(data.room).emit("is_typing",data)
+        socket.to(data.room).emit("is_typing",data)
     })
 
     socket.on("stop_typing",(data)=>{
@@ -43,7 +60,20 @@ io.on("connection",(socket)=>{ // event call when someone entires, each user has
 
     socket.on("disconnect",()=>{ // when someone disconnect the disconnect event call the socket.on runs
    
-        console.log("Naruto Left")
+        const index = users.findIndex(each => each.socketId === socket.id) // find's the index of disconnected user
+
+        if(index !== -1){
+            const disconnectedUser = users[index];
+            users.splice(index,1) // removes disconnected user from original array
+
+            const roomUsers = users.filter(each => each.room === disconnectedUser.room )
+
+            io.to(disconnectedUser.room).emit("room_users",roomUsers)
+            
+            console.log(`${disconnectedUser.username} left ${disconnectedUser.room}`)
+        } 
+
+        console.log("disconnected user",socket.id)
 })
 
 });
